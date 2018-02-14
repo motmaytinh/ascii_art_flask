237fe3d09c765c9d0e936a698cea57f83b3c5ca5
Quy Tran Ngoc <motmaytinh@gmail.com>
2018-02-14 10:35:20 +0700

    Save file manual

--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ def index():
                            title='Home')
 
 @app.route('/upload', methods=['POST', 'GET'])
-def upload_file():
+def upload():
     if request.method == 'POST':
         file = request.files['image']
         f = os.path.join("/" + app.config['UPLOAD_FOLDER'], file.filename)
@@ -24,7 +24,7 @@ def upload_file():
         # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
         file.save(f)
         
-        return render_template('index.html',
+        return render_template('show.html',
                            title='Home')
 
     return render_template('upload.html')
