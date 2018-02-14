6fda35f38a0b275f2498fde0a4d9dbc2fdf4b9a4
Quy Tran Ngoc <motmaytinh@gmail.com>
2018-02-14 15:28:30 +0700

    Fairly good

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from app import app
 from .ASCII_Art import ascii
 
 UPLOAD_FOLDER = os.path.dirname(__file__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER + "/static"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 @app.route('/')
@@ -26,10 +26,11 @@ def upload():
         
         # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
         file.save(f)
+        # o = os.path.join("/" + app.config['UPLOAD_FOLDER'], "out.txt")
         o = os.path.join("/" + app.config['UPLOAD_FOLDER'], "out.txt")
         ascii(f, o)
 
         return render_template('show.html',
-                           title='Home', path = o)
+                           title='Home')
 
     return render_template('upload.html')
